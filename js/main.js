7b079843127f31907db2a9c53222a5ddfb828a60
YukinoKingu <romangai00@gmail.com>
2021-05-07 14:51:28 +0800

    first try

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function validation(){
+  let  login_fields = document.querySelector("#login_field").value
+  let  password_field = document.querySelector('#password_field').value
+  alert( login_fields)
+  alert(password_field)
+}
